d5b300c9c51501d878b46a4cac50467df27ff585
mansao01 <novrilukman15@gmail.com>
2023-12-11 13:18:16 +0700

    add upload image endpoint

---
+++ modules/imgUpload.js
@@ -0,0 +1,50 @@
+'use strict'
+import {Storage} from "@google-cloud/storage";
+import dateFormat from "dateformat"
+import path from "path"
+
+const pathKey = path.resolve('./serviceaccountkey.json')
+
+// TODO: Sesuaikan konfigurasi Storage
+const gcs = new Storage({
+    projectId: 'coffeebid-capstone',
+    keyFilename: pathKey
+})
+
+// TODO: Tambahkan nama bucket yang digunakan
+const bucketName = 'capstone-coffee-bid'
+const bucket = gcs.bucket(bucketName)
+
+function getPublicUrl(filename) {
+    return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+}
+
+let ImgUpload = {}
+
+ImgUpload.uploadToGcs = (req, res, next) => {
+    if (!req.file) return next()
+
+    const gcsName = dateFormat(new Date(), "yyyymmdd-HHMMss")
+    const file = bucket.file(gcsName)
+
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
+        }
+    })
+
+    stream.on('error', (err) => {
+        req.file.cloudStorageError = err
+        next(err)
+    })
+
+    stream.on('finish', () => {
+        req.file.cloudStorageObject = gcsName
+        req.file.cloudStoragePublicUrl = getPublicUrl(gcsName)
+        next()
+    })
+
+    stream.end(req.file.buffer)
+}
+
+export default ImgUpload
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {addProduct, deleteProduct, getProducts} from "../controller/ProductController.js";
+import {addImageProduct, addProduct, deleteProduct, getProducts} from "../controller/ProductController.js";
 
 const router = express.Router();
 
 router.post("/v1/addProduct", addProduct)
+router.post("/v1/addImageProduct", addImageProduct)
 router.delete("/v1/deleteProduct/:id", deleteProduct)
 router.get("/v1/getProducts", getProducts)
 

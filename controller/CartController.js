e3a9659ff3948d0b1afc1dac697a8ce244f0ef84
mansao01 <novrilukman15@gmail.com>
2023-12-18 16:52:29 +0700

    add total price at cart

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,27 +1,48 @@
 import TransactionModel from "../models/TransactionModel.js";
 import CartModel from "../models/CartModel.js";
 import ProductModel from "../models/ProductModel.js";
-//
-//
+
+
 // export const addToCart = async (req, res) => {
-//     const {userId} = req.body;
+//     const { transactionId, userId } = req.body;
 //
 //     try {
-//         const newCart = await CartModel.create({
-//             userId: userId,
-//             status: "active"
+//         let [cart, created] = await CartModel.findOrCreate({
+//             where: { userId },
+//             defaults: { status: 'active' }
+//         });
+//
+//         if (created) {
+//             cart = cart[0];
+//         }
+//
+//         await TransactionModel.update(
+//             { cartId: cart.id },
+//             { where: { id: transactionId } }
+//         );
+//
+//         const itemsInCart = await TransactionModel.findAll({
+//             where: { cartId: cart.id },
+//             include: [{ model: ProductModel }] // Include ProductModel to fetch product details
 //         });
 //
 //         res.status(200).json({
 //             msg: 'Item added to cart successfully',
-//             newCart
+//             cartId: cart.id,
+//             items: itemsInCart.map(item => ({
+//                 id: item.id,
+//                 transactionDate: item.transactionDate,
+//                 confirmed: item.confirmed,
+//                 washStatus: item.washStatus,
+//                 imageUrl: item.imageUrl,
+//                 productDetail: item.product // Product details for each item
+//             }))
 //         });
 //     } catch (error) {
 //         console.error('Error adding item to cart:', error);
-//         res.status(400).json({msg: 'Failed to add item to cart', error});
+//         res.status(500).json({ msg: 'Failed to add item to cart' });
 //     }
 // };
-
 
 export const addToCart = async (req, res) => {
     const { transactionId, userId } = req.body;
@@ -43,20 +64,33 @@ export const addToCart = async (req, res) => {
 
         const itemsInCart = await TransactionModel.findAll({
             where: { cartId: cart.id },
-            include: [{ model: ProductModel }] // Include ProductModel to fetch product details
+            include: [{ model: ProductModel }]
         });
 
-        res.status(200).json({
-            msg: 'Item added to cart successfully',
-            cartId: cart.id,
-            items: itemsInCart.map(item => ({
+        let totalPrice = 0;
+
+        const itemsWithPrice = itemsInCart.map(item => {
+            const itemPrice = item.product ? item.product.price : 0;
+            totalPrice += itemPrice; // Accumulate item prices for total price calculation
+
+            return {
                 id: item.id,
                 transactionDate: item.transactionDate,
                 confirmed: item.confirmed,
                 washStatus: item.washStatus,
                 imageUrl: item.imageUrl,
-                productDetail: item.product // Product details for each item
-            }))
+                productDetail: item.product,
+            };
+        });
+
+        // Update the cart's total price
+        await CartModel.update({ totalPrice }, { where: { id: cart.id } });
+
+        res.status(200).json({
+            msg: 'Item added to cart successfully',
+            cartId: cart.id,
+            items: itemsWithPrice,
+            totalPrice: totalPrice // Include total price in the response
         });
     } catch (error) {
         console.error('Error adding item to cart:', error);

c9e30449ee0e3c9e8cdc6449cdc65eed9652cdd0
mansao01 <novrilukman15@gmail.com>
2023-12-18 08:47:24 +0700

    success show product detail

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,5 +1,6 @@
 import TransactionModel from "../models/TransactionModel.js";
 import CartModel from "../models/CartModel.js";
+import ProductModel from "../models/ProductModel.js";
 //
 //
 // export const addToCart = async (req, res) => {
@@ -23,47 +24,42 @@ import CartModel from "../models/CartModel.js";
 
 
 export const addToCart = async (req, res) => {
-    const {transactionId, userId} = req.body;
+    const { transactionId, userId } = req.body;
 
     try {
+        let [cart, created] = await CartModel.findOrCreate({
+            where: { userId },
+            defaults: { status: 'active' }
+        });
 
-        let cart = await CartModel.findOne({
-            where: {
-                userId: userId
-            }
-        })
-        if (!cart) {
-            await CartModel.create({
-                userId,
-                status: 'active'
-                // Other fields initialization if needed
-            });
+        if (created) {
+            cart = cart[0];
         }
 
-        const newCart = await CartModel.findOne({
-            where: {
-                userId: userId
-            }
-        })
-        const updatedTransaction = await TransactionModel.update(
-            {cartId: newCart.id},
-            {where: {id: transactionId}}
+        await TransactionModel.update(
+            { cartId: cart.id },
+            { where: { id: transactionId } }
         );
 
         const itemsInCart = await TransactionModel.findAll({
-                where: {
-                    cartId: cart.id
-                }
-            }
-        )
+            where: { cartId: cart.id },
+            include: [{ model: ProductModel }] // Include ProductModel to fetch product details
+        });
 
         res.status(200).json({
             msg: 'Item added to cart successfully',
-            cartId: updatedTransaction,
-            items: itemsInCart
+            cartId: cart.id,
+            items: itemsInCart.map(item => ({
+                id: item.id,
+                transactionDate: item.transactionDate,
+                confirmed: item.confirmed,
+                washStatus: item.washStatus,
+                imageUrl: item.imageUrl,
+                productDetail: item.product // Product details for each item
+            }))
         });
     } catch (error) {
         console.error('Error adding item to cart:', error);
-        res.status(400).json({msg: 'Failed to add item to cart'});
+        res.status(500).json({ msg: 'Failed to add item to cart' });
     }
-}
+};

042bb1a4346ed4e2e4939d9d6dacee8219cf4214
mansao01 <novrilukman15@gmail.com>
2024-01-07 19:10:35 +0700

    misplaced route updateUser

--- routes/RoleRoute.js
+++ routes/RoleRoute.js
@@ -1,12 +1,10 @@
 import express from "express";
 import {addRole, deleteRole} from "../controller/RoleController.js";
 import {verifyToken} from "../middleware/VerifyToken.js";
-import {updateUser} from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.post("/v1/addRole",addRole)
 router.delete("/v1/deleteRole/:id",verifyToken, deleteRole)
-router.patch("/v1/updateUser/:id", updateUser)
 
 export default router;

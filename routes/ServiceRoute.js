e9c283460b719676fef6852d0e60d4946300fd58
mansao01 <novrilukman15@gmail.com>
2024-04-16 22:54:48 +0700

    rename for get services endpoint

--- routes/ServiceRoute.js
+++ routes/ServiceRoute.js
@@ -13,7 +13,7 @@ router.post("/v1/addService", addService)
 router.post("/v1/addImageProduct", addImageProduct)
 router.delete("/v1/deleteService/:id", deleteService)
 router.get("/v1/servicesByCategory", getServicesByCategory)
-router.get("/v1/getServices", getServices)
+router.get("/v1/services", getServices)
 
 
 export default router;

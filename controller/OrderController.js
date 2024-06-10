6966717197c965ea5fc6ed2666e1dacb7aa88d66
mansao01 <novrilukman15@gmail.com>
2024-06-10 22:49:34 +0700

    create endpoint for offline order

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -48,6 +48,61 @@ export const addOrder = (req, res) => {
     });
 };
 
+export const getOfflineOrders = async (req, res) => {
+    const {serviceUserId} = req.query
+    try {
+
+        const orders = await OrderModel.findAll({
+            where: {userId: serviceUserId},
+            include: [{model: ServiceModel}]
+        });
+        return res.status(200).json({orders});
+    } catch (e) {
+        return res.status(500).json({error: e});
+    }
+}
+
+export const createOfflineOrder = async (req, res) => {
+    upload.single('image')(req, res, async (err) => {
+        if (err) {
+            return res.status(400).json({msg: err.message});
+        }
+        const serviceUserId = "445375fa-d577-49f5-bba4-f039c90f9134"
+        const {washStatus, serviceId} = req.body;
+        let imageUrl = '';
+
+        // Call ImgUpload function to upload to GCS
+        ImgUpload.uploadToGcs(req, res, async (err) => {
+            if (err) {
+                return res.status(400).json({msg: 'Failed to upload to GCS', error: err});
+            }
+
+            if (req.file && req.file.cloudStoragePublicUrl) {
+                imageUrl = req.file.cloudStoragePublicUrl;
+
+                try {
+                    // Create a new transaction using Sequelize's create method
+                    const order = await OrderModel.create({
+                        washStatus: washStatus,
+                        imageUrl: imageUrl,
+                        serviceId: serviceId,
+                        userId: serviceUserId
+                    });
+
+                    // If transaction is created successfully, send a success response
+                    return res.status(201).json({msg: 'Success add order', order: order});
+                } catch (error) {
+                    // If there's an error during creation, handle it and send an error response
+                    return res.status(500).json({msg: 'Failed to create transaction', details: error.message});
+                }
+            } else {
+                return res.status(400).json({msg: 'Image upload failed or URL not found'});
+            }
+        });
+    });
+
+}
+
 export const updateWashStatus = async (req, res) => {
     const {washStatus, orderId} = req.query;
     try {
--- routes/OrderRoute.js
+++ routes/OrderRoute.js
@@ -1,9 +1,17 @@
 import express from "express";
-import {addOrder, deleteOrder, updateWashStatus} from "../controller/OrderController.js";
+import {
+    addOrder,
+    createOfflineOrder,
+    deleteOrder,
+    getOfflineOrders,
+    updateWashStatus
+} from "../controller/OrderController.js";
 
 const router = express.Router();
 
 router.post("/v1/addOrder", addOrder)
+router.post("/v1/createOfflineOrder", createOfflineOrder)
+router.get("/v1/getOfflineOrder", getOfflineOrders)
 router.patch("/v1/updateWashStatus", updateWashStatus)
 router.delete("/v1/deleteOrder/:orderId", deleteOrder)
 
